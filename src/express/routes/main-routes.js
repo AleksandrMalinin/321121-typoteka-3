d4e287264a3fd54c4151df14a65d3b1d73bbbdd5
AleksandrMalinin <alexander.s.malinin@gmail.com>
2020-04-12 01:03:36 +0300

    Добавляет шаблон регистрации и логина

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -6,8 +6,8 @@ const mainRouter = new Router();
 
 // Определение `GET` маршрутов
 mainRouter.get(`/`, (req, res) => res.send(`/`));
-mainRouter.get(`/register`, (req, res) => res.send(`/register`));
-mainRouter.get(`/login`, (req, res) => res.send(`/login`));
+mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
+mainRouter.get(`/login`, (req, res) => res.render(`login`));
 mainRouter.get(`/search`, (req, res) => res.send(`/search`));
 mainRouter.get(`/my`, (req, res) => res.send(`/my`));
 mainRouter.get(`/categories`, (req, res) => res.send(`/categories`));
