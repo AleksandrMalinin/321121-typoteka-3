c9b737b62b9d4ec76cdb30c3f56cf03626dc60f4
AleksandrMalinin <alexander.s.malinin@gmail.com>
2020-04-13 00:15:24 +0300

    Доделывает шаблоны

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -5,11 +5,11 @@ const {Router} = require(`express`);
 const mainRouter = new Router();
 
 // Определение `GET` маршрутов
-mainRouter.get(`/`, (req, res) => res.send(`/`));
+mainRouter.get(`/`, (req, res) => res.render(`main`));
 mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`login`));
-mainRouter.get(`/search`, (req, res) => res.send(`/search`));
-mainRouter.get(`/my`, (req, res) => res.send(`/my`));
-mainRouter.get(`/categories`, (req, res) => res.send(`/categories`));
+mainRouter.get(`/search`, (req, res) => res.render(`search`));
+mainRouter.get(`/my`, (req, res) => res.render(`my`));
+mainRouter.get(`/categories`, (req, res) => res.render(`all-categories`));
 
 module.exports = mainRouter;
