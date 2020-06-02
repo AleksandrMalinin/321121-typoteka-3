3b5f3d0c0c911f2ee285ca92caaebe3cc3aeffbd
AleksandrMalinin <alexander.s.malinin@gmail.com>
2020-06-03 01:24:49 +0300

    Обновляет файл запуска приложения

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,47 +1,41 @@
+// отвечает за подготовку express-приложения и запуск сервера
+
+'use strict';
+
 const express = require(`express`);
-const {Router} = require(`express`);
-const postsRoute = new Router();
 const chalk = require(`chalk`);
-const fs = require(`fs`).promises;
-const {HttpCode} = require(`../../constants`);
+const {HttpCode, API_PREFIX} = require(`../../constants`);
+const routes = require(`../api`);
+const getMockData = require(`../lib/get-mock-data`);
 
 const DEFAULT_PORT = 8000;
-const FILENAME = `mocks.json`;
 
 const app = express();
-// передача middleware,
-// явное обозначение формата передаваемых данных
 app.use(express.json());
-
-// Определение маршрута,
-// чтение файла и получение данных.
-// При отсутствии файла возвращаем пустой массив.
-postsRoute.get(`/posts`, async(req, res) => {
-  try {
-    const fileContent = await fs.readFile(FILENAME);
-    const mocks = JSON.parse(fileContent);
-    res.json(mocks);
-  } catch (err) {
-    res.json([]);
-    console.log(err);
-  }
-});
-
-// Регистрирация маршрута.
-app.use(`/`, postsRoute);
+app.use(API_PREFIX, routes);
+app.use((req, res) => res
+  .status(HttpCode.NOT_FOUND)
+  .send(`Not found`));
 
 module.exports = {
   name: `--server`,
-  run(args) {
+  async run(args) {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
 
-    app.listen(port, (err) => {
-      if (err) {
-        return console.error(chalk.red(`Ошибка при создании сервера`, err));
-      }
+    try {
+      await getMockData();
 
-      return console.info(chalk.green(`Ожидаю соединений на ${port}`));
-    });
+      app.listen(port, (err) => {
+        if (err) {
+          return console.error(chalk.red(`Ошибка при создании сервера`, err));
+        }
+
+        return console.info(chalk.green(`Ожидаю соединений на ${port}`));
+      });
+    } catch (err) {
+      console.error(`Произошла ошибка: ${err.message}`);
+      process.exit(1);
+    }
   }
-}
+};
